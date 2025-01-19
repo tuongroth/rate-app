58f30191196fe25f430dbd1566e3f8743dbb61aa
TuongRoth <rothtrancattuong0703@gmail.com>
2025-01-19 19:05:21 +0200

    Add Main component and integrate into App.js

---
+++ rate-repository-app/App.js
@@ -0,0 +1,12 @@
+// App.js or wherever you're using Main
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import Main from './src/components/Main';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Main />
+    </SafeAreaView>
+  );
+}
